c5a098a9943f22c4e993d9a7a045cc36fcebed84
Raze010 <RBOSSOR@gmail.com>
2022-03-17 14:22:30 +0100

    Avant interface

--- TP3_Interfaces/ClassesMetier/Port.cs
+++ TP3_Interfaces/ClassesMetier/Port.cs
@@ -11,10 +11,25 @@ namespace NavireHeritage.classesMetier
         private string nom, latitude, longitude;
         private int nbPortique, nbQuaisPassager, nbQuaisTanker, nbQuaisSuperTanker;
 
+        public string Nom { get => nom; }
+        public string Latitude { get => latitude; }
+        public string Longitude { get => longitude; }
+        public int NbPortique { get => nbPortique; set => nbPortique = value; }
+        public int NbQuaisPassager { get => nbQuaisPassager; set => nbQuaisPassager = value; }
+        public int NbQuaisTanker { get => nbQuaisTanker; set => nbQuaisTanker = value; }
+        public int NbQuaisSuperTanker { get => nbQuaisSuperTanker; set => nbQuaisSuperTanker = value; }
+
+
         private Dictionary<String, Navire> navireAttendus = new Dictionary<string, Navire>();
         private Dictionary<String, Navire> navireArrives = new Dictionary<string, Navire>();
         private Dictionary<String, Navire> navirePartis = new Dictionary<string, Navire>();
         private Dictionary<String, Navire> navireEnAttente = new Dictionary<string, Navire>();
+
+        public Dictionary<string,Navire> NavireAttendus { get => navireAttendus; }
+        public Dictionary<string, Navire> NavireArrives { get => navireArrives; }
+        public Dictionary<string, Navire> NavirePartis { get => navirePartis; }
+        public Dictionary<string, Navire> NavireEnAttente { get => navireEnAttente; }
+
 
         public Port(string nom, string latitude, string longitude, int nbPortique, int nbQuaisPassager, int nbQuaisTanker, int nbQuaisSuperTanker)
         {
@@ -89,15 +104,122 @@ namespace NavireHeritage.classesMetier
             return navireAttendus.ContainsKey(imo);
         }
 
-        public void Chargement(string imo, int nbDecharger)
+        public void Chargement(string imo, int nbCharger)
         {
             if(navireArrives.ContainsKey(imo))
             {
-                if(navireArrives[imo] is Cargo)
+                if(navireArrives[imo].TonnageActuel + nbCharger > navireArrives[imo].TonnageDWT)
                 {
-                    //navireArrives[imo].d
+                    navireArrives[imo].TonnageActuel += nbCharger;
+                }
+                else
+                {
+                    throw new Exception("Ce navire n'a pas assez de capacité de stockage");
+                }
+            } 
+            else
+            {
+                throw new Exception("Ce navire n'est pas dans navireArrivee");
+            }
+        }
+        public void DeChargement(string imo, int nbDecharger)
+        {
+            if (navireArrives.ContainsKey(imo))
+            {
+                if (navireArrives[imo].TonnageActuel - nbDecharger >= 0)
+                {
+                    navireArrives[imo].TonnageActuel -= nbDecharger;
+                }
+                else
+                {
+                    throw new Exception("La capacité d'un navire ne peut pas atteindre 0");
                 }
             }
+            else
+            {
+                throw new Exception("Ce navire n'est pas dans navireArrivee");
+            }
+        }
+
+        public Navire GetUnAttendu (string id )
+        {
+            if(navireArrives.ContainsKey(id))
+            {
+                return navireArrives[id];
+            }
+            else
+            {
+                throw new Exception("Il n'y a pas ce navire dans navireArrives");
+            }
+        }
+
+        public Navire GetUnArrive(string id)
+        {
+            if (navireArrives.ContainsKey(id))
+            {
+                return navireArrives[id];
+            }
+            else
+            {
+                throw new Exception("Il n'y a pas ce navire dans navireArrives");
+            }
+        }
+
+        public Navire GetUnParti(string id)
+        {
+            if (navirePartis.ContainsKey(id))
+            {
+                return navireArrives[id];
+            }
+            else
+            {
+                throw new Exception("Il n'y a pas ce navire dans navireArrives");
+            }
+        }
+
+        public int GetNbTankerArrives()
+        {
+            int nb = 0;
+
+            foreach(Navire n in navireArrives.Values)
+            {
+                if(n.TonnageGT <= 130000)
+                {
+                    nb++;
+                }
+            }
+
+            return nb;
+        }
+
+        public int GetNbSuperTankerArrives()
+        {
+            int nb = 0;
+
+            foreach (Navire n in navireArrives.Values)
+            {
+                if (n.TonnageGT > 130000)
+                {
+                    nb++;
+                }
+            }
+
+            return nb;
+        }
+
+        public int GetNbCargoArrives()
+        {
+            int nb = 0;
+
+            foreach (Navire n in navireArrives.Values)
+            {
+                if (n is Cargo)
+                {
+                    nb++;
+                }
+            }
+
+            return nb;
         }
     }
 }
---
+++ TP3_Interfaces/ClassesMetier/Stockage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavireHeritage.classesMetier
+{
+    class Stockage : Navire
+    {
+        private int numero;
+        private int capaciteMaxi,capaciteDispo;
+
+        public int Numero { get => numero; }
+        public int CapaciteMaxi { get => capaciteDispo; set => capaciteDispo = value; }
+        public int CapaciteDispo { get => capaciteMaxi; set => capaciteMaxi = value; }
+
+
+        public Stockage(string imo, string nom, string latitude, string longitude, int tonnageActuel, int tonnageGT, int tonnageDWT,int numero,int capaciteMaxi) 
+            : base(imo, nom, latitude, longitude, tonnageActuel, tonnageGT, tonnageDWT)
+        {
+            this.numero = numero;
+            this.capaciteDispo = capaciteMaxi;
+            this.capaciteMaxi = capaciteMaxi;
+        }
+    }
+}
